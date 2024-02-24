df4f96aa82f541e91720abecf7a6db1a83650f97
Samuel Zweifel <zweifelsa+github@gmail.com>
2024-02-24 22:01:54 +0100

    Add 'sick' functionality

--- punch.py
+++ punch.py
@@ -3,11 +3,10 @@
 import argparse
 
 from datetime_parser import parse_time_span, parse_date
-from add import add
+from add import add, add_sick
 from hours import hours
 from overtime import overtime
 from remove import remove
-
 
 def add_year_argument(parser):
     parser.add_argument('-y', '--year', type=int, choices=range(2021, 3000), metavar="2021+",
@@ -21,6 +20,12 @@ def add_week_argument(parser):
 def arguments_add(parser):
     parser.add_argument('time', type=parse_time_span, help="Time span like HH-HH or HH:mm-HH:mm")
     parser.add_argument('-d', '--date', type=parse_date, help="Date of the day logged as YYYY-MM-DD")
+    parser.add_argument('-p', '--prefix', choices=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                        help="Prefix of log line,describing the weekday.")
+
+
+def arguments_sick(parser):
+    parser.add_argument('-d', '--date', type=parse_date, help="Start date of sickness as YYYY-MM-DD")
     parser.add_argument('-p', '--prefix', choices=['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
                         help="Prefix of log line,describing the weekday.")
 
@@ -46,6 +51,7 @@ def parse_args():
     arguments_remove(sub_p.add_parser('remove', help="Remove lines"))
     arguments_hours(sub_p.add_parser('hours', help="Calculate weekly hours"))
     arguments_overtime(sub_p.add_parser('overtime', help="Calculate overtime"))
+    arguments_sick(sub_p.add_parser('sick', help="Add sick day"))
 
     return parser.parse_args()
 
@@ -56,7 +62,10 @@ if __name__ == '__main__':
         remove(args.num)
     elif args.operation == 'add':
         add(args.date, args.time, args.prefix)
+    elif args.operation == 'sick':
+        add_sick(args.date, args.prefix)
     elif args.operation == 'hours':
         hours(args.year, args.week)
     elif args.operation == 'overtime':
         overtime(args.week)
+
