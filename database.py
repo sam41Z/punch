e4710bb78d1f406d4e83f6970cb9d2e5b66f7b84
Samuel Zweifel <zweifelsa+github@gmail.com>
2024-02-24 22:02:45 +0100

    Fix Timezone

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
-from datetime import datetime
-from sqlalchemy import create_engine
-from sqlalchemy import TIMESTAMP
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
+from datetime import datetime, timezone
 from os.path import expanduser
+
+from sqlalchemy import create_engine, TypeDecorator, Float, Integer
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
 
 class Database:
@@ -18,9 +18,24 @@ class Database:
         return cls(url="sqlite://")
 
 
+# define a custom type for unix timestamp
+class UnixTimestamp(TypeDecorator):
+    # convert unix timestamp to datetime object
+    impl = Integer
+    cache_ok = True
+
+    # convert datetime object to unix timestamp when inserting data to database
+    def process_bind_param(self, value: datetime, dialect):
+        return int(value.timestamp())
+
+    # convert unix timestamp to  datetime object retrieving data from database
+    def process_result_value(self, value, dialect):
+        return datetime.fromtimestamp(value).astimezone()
+
+
 class Base(DeclarativeBase):
     type_annotation_map = {
-        datetime: TIMESTAMP(timezone=True),
+        datetime: UnixTimestamp,
     }
 
 
