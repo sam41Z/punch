e864f1c7bd4a143b1baac8f3e24f3ace78c8d184
Samuel Zweifel <zweifelsa+github@gmail.com>
2024-02-24 22:01:54 +0100

    Reorganize holidays

--- holidays.py
+++ holidays.py
@@ -1,17 +1,8 @@
-import os
 from datetime import timedelta
-from files import get_base_path
 
 
 def num_holidays(year, week):
-    file_name = get_file_name(year)
-    file = open(file_name, 'r')
-    for line in file:
-        week_str, num_str = line.split(' ', 1)
-
-        if int(week_str) == week:
-            return float(num_str)
-    return 0.0
+    return HOLIDAYS[int(year)].get(int(week), 0.0)
 
 
 def weekly_and_holidays(year, week):
@@ -19,5 +10,7 @@ def weekly_and_holidays(year, week):
     return timedelta(hours=(32 / 5 * (5 - holidays))), holidays
 
 
-def get_file_name(year):
-    return "{0}/{1}/holidays".format(get_base_path(), year)
+HOLIDAYS = {
+    2023: {1: 1, 14: 1, 15: 1, 16: 0.5, 18: 1, 20: 1, 22: 1, 31: 1, 37: 0.5, 52: 2},
+    2024: {1: 2, 13: 1, 14: 1, 16: 0.5, 18: 1, 19: 1, 21: 1, 31: 1, 37: 0.5, 52: 2.5}
+}
